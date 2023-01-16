d0171fd29c622b7f1c6e6b2c13016d4541797917
Mohammed <ezio.ahmed12@gmail.com>
2023-01-16 19:12:04 +0300

    mohammed:upload-files

---
+++ main.js
@@ -0,0 +1,28 @@
+const {app,BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+function createBrowserWindow(){
+    const mainWindow = new BrowserWindow({
+        title :'Electron App',
+
+        width: 1000,
+
+        height: 600,
+
+
+    });
+    const startUrl = url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protoco: 'file',
+
+    });
+
+
+    mainWindow.loadURL(startUrl);
+
+}
+
+
+app.whenReady().then(createBrowserWindow);
+
